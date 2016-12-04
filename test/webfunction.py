a4203de97bef5a33bb8fe58fd0520f612f4f17ac
himanis <himanisingh.sh@gmail.com>
2016-12-03 21:34:40 -0800

    adding test folder

---
+++ test/webfunction.py
@@ -0,0 +1,8 @@
+# import test_module
+#import python.Libs.test_module
+import sys
+sys.path.append("C:\Users\himan\Documents\Himani-Data\Git-Working\python")
+import python.function.Web_related_functions as webfxn
+
+if __name__== "__main__":
+    print "hello"
