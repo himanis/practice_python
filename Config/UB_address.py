4ce6343d33a63730c750425f836591a22456229c
himanis <himanisingh.sh@gmail.com>
2017-02-22 17:11:50 -0800

    readding

---
+++ Config/UB_address.py
@@ -0,0 +1,6 @@
+__author__ = 'himani'
+
+network = {'IP'=192.168.1.100', 'gw':'192.168.1.1','netmask':'255.255.255.0'}
+cli_single_cmd = [  'ping yahoo.com',
+                  'top '
+                    ]
